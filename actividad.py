b55db6275fa997bca55c335ed8aee467052ed8d9
luxorR6 <a20200068@utem.edu.mx>
2023-06-07 20:51:33 -0600

    test de pullrequest
    Signed-off-by: luxorR6 <a20200068@utem.edu.mx>

---
+++ actividad.py
@@ -0,0 +1,10 @@
+# Archivo: hola_servidor.py
+
+# Imprime el mensaje "Hola, desde Servidor"
+print("Hola, desde Servidor")
+
+# Declara la variable 'nombre' y asígnale tu nombre como valor
+nombre = "Eduardo Ortiz"
+
+# Imprime el valor de la variable 'nombre'
+print(nombre)
